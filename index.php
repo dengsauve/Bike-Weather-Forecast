1394c0500767ca177b069b32eb5ed035a1c30c39
Dennis Sauve <dengsauve@yahoo.com>
2018-04-20 00:10:21 -0700

    Created Home Page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bikercast</title>
+
+        <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.css" />
+
+    </head>
+    <body>
+        <div class="container">
+
+        <h1>Hello World</h1>
+
+        <h2>Weather Today:</h2>
+
+        <p class="help-block">
+            cloudy
+        </p>
+
+        <script type="text/javascript" src="/node_modules/jquery/dist/jquery.js"></script>
+        <script type="text/javascript" src="/node_modules/bootstrap/dist/bootstrap.js" ></script>
+    </body>
+</html>
