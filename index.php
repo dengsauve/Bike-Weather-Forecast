35a53dd8f67d3e8f8b87f1694bbcead4b3129efb
Dennis Sauve <dengsauve@yahoo.com>
2018-05-12 15:58:47 -0700

    Conditional weather warning colors

--- index.php
+++ index.php
@@ -1,14 +1,67 @@
 <?php
+    // Current Conditions and Ten Day Forecast URLs
     $cc_url = "http://api.wunderground.com/api/963b29bf0454809e/conditions/q/WA/Spokane.json";
+    $fc_url = "http://api.wunderground.com/api/963b29bf0454809e/forecast/q/WA/Spokane.json";
     $ten_url = "http://api.wunderground.com/api/963b29bf0454809e/forecast10day/q/WA/Spokane.json";
 
     $cc_json = file_get_contents($cc_url);
+    $fc_json = file_get_contents($fc_url);
     $ten_json = file_get_contents($ten_url);
 
     $cc = json_decode($cc_json);
+    $fc = json_decode($fc_json);
     $ten = json_decode($ten_json);
 
+    // The Wunderground Logo url
     $img_url = $cc->{'current_observation'}->{'image'}->{'url'};
+
+    // Conditional Alert Strings
+    $great = "success";
+    $clear = "info";
+    $subpar = "warning";
+    $poor = "danger";
+
+    // Today's Forecast
+    $fc_day = $fc->{'forecast'}->{'txt_forecast'}->{'forecastday'}[0]->{'fcttext'};
+
+    $neg_keywords = ['thunderstorm', 'snow', 'rain', 'freezing', 'shower', 'flurries', 'cloudy', 'variable'];
+    $pos_keywords = ['sunny', 'light', 'sunshine', 'clear'];
+
+    $score = 0;
+
+    foreach($neg_keywords as $n)
+    {
+        if( strpos($fc_day, $n) !== false ){
+            $score--;
+        }
+    }
+
+    foreach($pos_keywords as $p)
+    {        
+        if( strpos($fc_day, $p) !== false ){
+            $score++;
+        }
+    }
+
+    $condition = $clear;
+
+    if( $score > 0 )
+    {
+        $condition = $great;
+    }
+    elseif( $score == 0 )
+    {
+        $condition = $clear;
+    }
+    elseif( $score < 0 )
+    {
+        $condition = $subpar;
+    }
+    else
+    {
+        $condition = $poor;
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +69,8 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="subject" content="weather forecast" />
+        <meta name="author" content="Dennis Sauve, dengsauve@yahoo.com" />
 
         <title>bikercast</title>
         <meta name="description" content="Providing critical weather forecast information to determine if biking into work is a good idea." />
@@ -27,10 +82,15 @@
     <body>
         <div class="container">
             <div class="row">
-                <h1 class="display-4 col-12">bikercast</h1>
-                <p class="lead col-12">
-                    Providing critical weather forecast information to determine if biking into work is a good idea.
-                </p>
+                <section class="col-12">
+                    <h1 class="display-4">bikercast</h1>
+                    <p class="lead">
+                        Providing critical weather forecast information to determine if biking into work is a good idea.
+                    </p>
+                    <div class="alert alert-<?php echo $condition; ?>" role="alert">
+                        <?php echo $fc_day; ?>
+                    </div>
+                </section>
             </div>
 
             <div class="row">
